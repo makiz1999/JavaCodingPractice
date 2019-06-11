773f2ce75429dccedc28faecfeac199329f8998f
Max <49218301+makiz1999@users.noreply.github.com>
2019-06-10 23:45:14 -0500

    Create practiceCode2.java

---
+++ practiceCode2.java
@@ -0,0 +1,26 @@
+public class HW1 {
+    public static boolean hasSameLastDigit( int num1, int num2, int num3) {
+        if ((num1 < 10 || num1 > 1000) || (num2 < 10 || num2 > 1000) || (num3 < 10 || num3 > 1000)){
+            return false;
+        }
+        int num1LastDigit = num1 % 10;
+        int num2LastDigit = num2 % 10;
+        int num3LastDigit = num3 % 10;
+        if ((num1LastDigit == num2LastDigit) || (num1LastDigit == num3LastDigit) ||
+                (num2LastDigit == num3LastDigit)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static boolean isValid ( int number ){
+        if ( number < 10 || number > 1000){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    public static void main(String[] args){
+    }
+}
+
